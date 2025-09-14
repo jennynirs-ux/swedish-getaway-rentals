1b9585610a6518f3259852bd262062480f4ca061
jennynirs-ux <jenny.nirs@mojjo.se>
2025-09-14 10:42:03 +0200

    Update PropertyPage.tsx

--- src/pages/PropertyPage.tsx
+++ src/pages/PropertyPage.tsx
@@ -5,7 +5,7 @@ import { Property } from "@/hooks/useProperties";
 import { useOptimizedQuery } from '@/hooks/useOptimizedQuery';
 import PropertyNavigation from "@/components/PropertyNavigation";
 import PropertyHero from "@/components/PropertyHero";
-import PropertyGalleryOptimized from "@/components/PropertyGalleryOptimized";
+import PropertyGalleryOptimized from "@/components/PropertyGallery";
 import PropertyAmenities from "@/components/PropertyAmenities";
 import PropertySpecialHighlights from "@/components/PropertySpecialHighlights";
 import PropertyBooking from "@/components/PropertyBooking";
@@ -17,7 +17,7 @@ import { ArrowLeft } from "lucide-react";
 
 // Memoized components for better performance
 const MemoizedPropertyHero = memo(PropertyHero);
-const MemoizedPropertyGalleryOptimized = memo(PropertyGalleryOptimized);
+const MemoizedPropertyGallery = memo(PropertyGallery);
 const MemoizedPropertyAmenities = memo(PropertyAmenities);
 const MemoizedPropertySpecialHighlights = memo(PropertySpecialHighlights);
 const MemoizedPropertyBooking = memo(PropertyBooking);
