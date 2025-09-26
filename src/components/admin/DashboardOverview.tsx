a129ea7117691503c3c88ec234aeb6c6d0aed25f
jennynirs-ux <jenny.nirs@mojjo.se>
2025-09-26 19:26:18 +0200

    Update DashboardOverview.tsx

--- src/components/admin/DashboardOverview.tsx
+++ src/components/admin/DashboardOverview.tsx
@@ -210,8 +210,6 @@ const DashboardOverview = ({ onNavigateToTab, onEditProperty, onEditProduct }: D
 
   return (
     <div className="space-y-6">
-      return (
-  <div className="space-y-6">
     {/* Overview Cards */}
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
       <Card>
